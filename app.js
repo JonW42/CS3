44e6d066c42b3e6674d07dd2a182fd93b9491df5
JonW42 <Jonathan.W42@gmail.com>
2016-03-07 23:36:14 -0800

    Added files via upload

--- app.js
+++ app.js
@@ -3,20 +3,26 @@ var main = function () {
     
     $('.navbtnS').click(function(){
         console.log("navS click");
-        $('.navhide').hide();
-        $('.society').show();
+        $('.navhide').fadeOut("fast");
+        $('.society').fadeIn("slow");
     })
     
     $('.navbtnE').click(function(){
         console.log("navE click");
-        $('.navhide').hide();
-        $('.events').show();
+        $('.navhide').fadeOut("fast");
+        $('.events').fadeIn("slow");
     })
     
     $('.navbtnP').click(function(){
         console.log("navP click");
-        $('.navhide').hide();
-        $('.people').show();
+        $('.navhide').fadeOut("fast");
+        $('.people').fadeIn("slow");
+    })
+    
+    $('.navbtnC').click(function(){
+        console.log("navC click");
+        $('.navhide').fadeOut("fast");
+        $('.contact').fadeIn("slow");
     })
 
     
