f6a6f074f2063385f04e5bba9262367da1cc1fc1
JonW42 <Jonathan.W42@gmail.com>
2016-03-08 00:23:42 -0800

    Added files via upload

--- app.js
+++ app.js
@@ -2,30 +2,46 @@ var main = function () {
  
     
     $('.navbtnS').click(function(){
-        console.log("navS click");
         $('.navhide').fadeOut("fast");
         $('.society').fadeIn("slow");
     })
     
     $('.navbtnE').click(function(){
-        console.log("navE click");
         $('.navhide').fadeOut("fast");
         $('.events').fadeIn("slow");
     })
     
     $('.navbtnP').click(function(){
-        console.log("navP click");
         $('.navhide').fadeOut("fast");
         $('.people').fadeIn("slow");
     })
     
     $('.navbtnC').click(function(){
-        console.log("navC click");
         $('.navhide').fadeOut("fast");
         $('.contact').fadeIn("slow");
     })
 
     
 };
+
+//**not working** code i stole from here: http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
+var idAnime = function() {
+          $('a[href*=#]').each(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            && location.hostname == this.hostname
+            && this.hash.replace(/#/,'') ) {
+              var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+              var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+               if ($target) {
+                 var targetOffset = $target.offset().top;
+                 $(this).click(function() {
+                   $('html, body').animate({scrollTop: targetOffset}, 4000);
+                   return false;
+                 });
+              }
+            }
+          });
+        };
+
 
 $(document).ready(main);
