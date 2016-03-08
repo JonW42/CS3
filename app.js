5abb5b372c30dbb5ff6cd7a2ddcb950e1b15424e
JonW42 <Jonathan.W42@gmail.com>
2016-03-07 18:24:13 -0800

    Added files via upload

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var main = function () {
-   
+ 
+    
+    $('.navbtnS').click(function(){
+        console.log("navS click");
+        $('.navhide').hide();
+        $('.society').show();
+    })
+    
+    $('.navbtnE').click(function(){
+        console.log("navE click");
+        $('.navhide').hide();
+        $('.events').show();
+    })
+    
+    $('.navbtnP').click(function(){
+        console.log("navP click");
+        $('.navhide').hide();
+        $('.people').show();
+    })
 
     
 };
